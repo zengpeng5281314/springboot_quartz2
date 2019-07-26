16d80f5a124638db3bf3335420221468a39ad723
zengpeng5281314 <zengpeng5281314@163.com>
2019-07-26 17:45:09 +0800

    a

--- src/main/java/com/channel/zengpeng/service/QuartzConfigurer.java
+++ src/main/java/com/channel/zengpeng/service/QuartzConfigurer.java
@@ -14,29 +14,30 @@ import org.springframework.scheduling.quartz.SchedulerFactoryBean;
 
 @Configuration
 public class QuartzConfigurer {
+	@Autowired
+	DataSource dataSource;
+	@Autowired
+	TaskJobFactory jobFactory;
 
-    @Autowired
-    TaskJobFactory jobFactory;
+	@Bean(name = "SchedulerFactory")
+	public SchedulerFactoryBean schedulerFactoryBean() throws IOException {
+		// 获取配置属性
+		PropertiesFactoryBean propertiesFactoryBean = new PropertiesFactoryBean();
+		propertiesFactoryBean.setLocation(new ClassPathResource("/quartz.properties"));
+		// 在quartz.properties中的属性被读取并注入后再初始化对象
+		propertiesFactoryBean.afterPropertiesSet();
+		// 创建SchedulerFactoryBean
+		SchedulerFactoryBean factory = new SchedulerFactoryBean();
+		factory.setQuartzProperties(propertiesFactoryBean.getObject());
+		factory.setJobFactory(jobFactory);
+		return factory;
+	}
 
-    @Bean(name = "SchedulerFactory")
-    public SchedulerFactoryBean schedulerFactoryBean() throws IOException {
-        //获取配置属性
-        PropertiesFactoryBean propertiesFactoryBean = new PropertiesFactoryBean();
-        propertiesFactoryBean.setLocation(new ClassPathResource("/quartz.properties"));
-        //在quartz.properties中的属性被读取并注入后再初始化对象
-        propertiesFactoryBean.afterPropertiesSet();
-        //创建SchedulerFactoryBean
-        SchedulerFactoryBean factory = new SchedulerFactoryBean();
-        factory.setQuartzProperties(propertiesFactoryBean.getObject());
-        factory.setJobFactory(jobFactory);
-        return factory;
-    }
-
-    /*
-     * 通过SchedulerFactoryBean获取Scheduler的实例
-     */
-    @Bean(name = "scheduler")
-    public Scheduler scheduler() throws IOException {
-        return schedulerFactoryBean().getScheduler();
-    }
+	/*
+	 * 通过SchedulerFactoryBean获取Scheduler的实例
+	 */
+	@Bean(name = "scheduler")
+	public Scheduler scheduler() throws IOException {
+		return schedulerFactoryBean().getScheduler();
+	}
 }
